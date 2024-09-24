fd6c653b96a6eba9a32642e9b56e3b5f74e46d8b
nebjamin <jonb1@newpaltz.edu>
2024-09-23 22:18:25 -0400

    Complete Step 3

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -10,6 +10,27 @@ const App = () => {
     setCount(count + multiplier)
   }
 
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setCount(count - 10)
+      setMultiplier(multiplier + 1)
+    }
+  }
+
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 5);
+      setCount(count - 100);
+    }
+  }
+
+  const buyFullFeast = () => {
+    if (count >= 1000) {
+      setMultiplier( multiplier * 10);
+      setCount(count - 1000);
+    }
+  }
+
   return (
     <div className="App">
       <div className="header">
@@ -22,6 +43,23 @@ const App = () => {
           onClick={updateCount}
         />
 
+      </div>
+      <div className="container">
+        <div className="upgrade">
+          <h3>Double Stuffed 👯‍♀️</h3>
+          <p>2x per click</p>
+          <button onClick={buyDoubleStuffed}>10 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Party Pack 🎉</h3>
+          <p>5x per click</p>
+          <button onClick={buyPartyPack}>100 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Full Feast 👩🏽‍🍳</h3>
+          <p>10x per click</p>
+          <button onClick={buyFullFeast}>1000 samosas</button>
+        </div>
       </div>
     </div>
   )
