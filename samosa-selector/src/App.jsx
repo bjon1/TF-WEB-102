4135519072f89d53c2942e21e53205e0caa09e36
nebjamin <jonb1@newpaltz.edu>
2024-09-23 21:47:09 -0400

    Complete Step 1

---
+++ samosa-selector/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count:</h2>
+        <img 
+          src="https://helpful-griffin-0a0ba5.netlify.app/samosa.png" 
+          alt="" 
+          className="samosa" 
+        />
+        
+      </div>
+    </div>
+  )
+
+}
+
+export default App
