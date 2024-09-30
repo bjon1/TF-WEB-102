a6f9475444b55c3376f5581a99f864920a950229
nebjamin <jonb1@newpaltz.edu>
2024-09-29 22:25:38 -0400

    Lab 3 Step 1

--- barista-app/src/App.jsx
+++ barista-app/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import BaristaForm from './components/BaristaForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <div>
+        <h1 className="title">On My Grind</h1>
+        <p>So you think you can barista? Let's put that to the test...</p>
+      </div>
+      <BaristaForm />
     </>
   )
 }
