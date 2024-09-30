e6adf792a1d37306078f6763e8db16f4e4473501
nebjamin <jonb1@newpaltz.edu>
2024-09-29 22:45:08 -0400

    Lab 3 Step 2

--- barista-app/src/components/BaristaForm.jsx
+++ barista-app/src/components/BaristaForm.jsx
@@ -1,6 +1,21 @@
 import {Component, useState} from "react";
+import RecipeChoices from "./RecipeChoices";
 
 const BaristaForm = () => {
+
+    const [inputs, setInputs] = useState({
+        'temperature': '',
+        'milk': '',
+        'syrup': '',
+        'blended': ''
+    });
+
+    const ingredients = {
+        'temperature' : ['hot', 'lukewarm', 'cold'],
+        'syrup': ['mocha', 'vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
+        'milk': ['cow', 'oat', 'goat', 'almond', 'none'],
+        'blended': ['yes', 'turbo', 'no']
+    }
 
     const onNewDrink = () => {
 
@@ -25,6 +40,24 @@ const BaristaForm = () => {
                     onClick={onNewDrink}
                 >New Drink</button>
             </form>
+
+            {Object.keys(ingredients).map((ingredient) => (
+                <div key={ingredient}>
+                    <h3>{ingredient.charAt(0).toUpperCase() + ingredient.slice(1)}</h3>
+                    <div className="answer-space">
+                        {inputs[ingredient]} 
+                    </div>
+                    <RecipeChoices
+                        handleChange={(e) => setInputs((prevState) => ({
+                            ...prevState,
+                            [e.target.name]: e.target.value,
+                        }))}
+                        label={ingredient}
+                        choices={ingredients[ingredient]}
+                        checked={inputs[ingredient]}
+                    />
+                </div>
+            ))}
         </div>
     );
   
