5b9326df6b2652e6d2c27c396cb251dbe669704d
nebjamin <jonb1@newpaltz.edu>
2024-10-08 01:00:43 -0400

    Complete Lab 4 Step 1

--- cap/src/App.jsx
+++ cap/src/App.jsx
@@ -1,15 +1,40 @@
 import { useState } from 'react'
 import './App.css'
 
+import APIForm from './components/APIForm'
+
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const submitForm = () => {
+    
+  }
+
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: ""
+  })
 
   return (
-    <>
-
-    </>
+    <div className="whole-page">
+      <h1>Build Your Own Screenshot! 📸</h1>
+      <APIForm
+        inputs={inputs}
+        handleChange={(e) => 
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm}
+      />
+      <br></br>
+    </div>
   )
 }
 
