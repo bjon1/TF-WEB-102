f6cf8cd82476eecbd430046c435df893dadbe1f5
nebjamin <jonb1@newpaltz.edu>
2024-10-14 22:29:06 -0400

    Lab 5 Step 1 Complete

--- crypto-hustle/src/App.jsx
+++ crypto-hustle/src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
-function App() {
+const App = () => {
+
+  const [ list, setList ] = useState(null)
+
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch("https://min-api.cryptocompare.com/data/all/coinlist?api_key=$" + API_KEY) 
+      const data = await response.json()
+      setList(data)
+    }
+    fetchAllCoinData().catch(console.error)
+  }, [])
 
   return (
-    <>
-
-    </>
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+      <ul>
+        {list && Object.entries(list.Data).map(([coin]) =>
+          list.Data[coin].PlatformType === "blockchain" ? (
+              <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+            ) : null
+        )}
+          
+      </ul>
+    </div>
   )
 }
 
