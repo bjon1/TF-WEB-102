c582b78aac87793ce7ca32b12416b06ce5087095
nebjamin <jonb1@newpaltz.edu>
2024-10-14 22:43:12 -0400

    Lab 5 Step 3 Complete

--- crypto-hustle/src/components/CoinInfo.jsx
+++ crypto-hustle/src/components/CoinInfo.jsx
@@ -7,10 +7,14 @@ const CoinInfo = ({image, name, symbol}) => {
 
     useEffect(() => {
         const getCoinPrice = async () => {
-            const response = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD&api_key=${API_KEY}`)
-            const data = await response.json()
-            setPrice(data)
-        }
+            const response = await fetch(
+              `https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD&api_key=` +
+                API_KEY
+            )
+          
+            const json = await response.json();
+            setPrice(json);
+          }
         getCoinPrice().catch(console.error)
     }, [symbol])
 
