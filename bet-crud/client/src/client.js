6f2ace7e9871efad9f2867b9ea0712ecd6cfa7ac
nebjamin <jonb1@newpaltz.edu>
2024-10-27 18:48:58 -0400

    Expose API Keys

--- bet-crud/client/src/client.js
+++ bet-crud/client/src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const URL = import.meta.env.VITE_SUPABASE_URL;
-const API_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
+const URL = 'https://piihrwmjnwallqgdsjjk.supabase.co';
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpaWhyd21qbndhbGxxZ2RzamprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwNjA1NDgsImV4cCI6MjA0NTYzNjU0OH0.5yGn4KCKojSQkV4NimCIylDKo-4CcuoWeoLeR44I-2A';
 
 export const supabase = createClient(URL, API_KEY);
