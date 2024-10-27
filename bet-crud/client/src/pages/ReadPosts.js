0ca467922b7797d54a32d1cb4198e0eaa9c749c1
nebjamin <jonb1@newpaltz.edu>
2024-10-27 18:44:54 -0400

    Lab 7 Complete

---
+++ bet-crud/client/src/pages/ReadPosts.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import Card from '../components/Card';
+import { supabase } from '../client'
+
+const ReadPosts = (props) => {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const { data } = await supabase
+                .from('Posts')
+                .select()
+                .order('created_at', { ascending: true })
+            setPosts(data);
+        }
+        fetchPosts();
+    }, [props]);
+    
+    return (
+        <div className="ReadPosts">
+            {
+                posts && posts.length > 0 ?
+                posts.map((post, index) => 
+                   <Card id={post.id} key={index} title={post.title} author={post.author} description={post.description}/>
+                ) : <h2>{'No Challenges Yet 😞'}</h2>
+            }
+        </div>  
+    )
+}
+
+export default ReadPosts;
